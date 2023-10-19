8d5b14eedd74dc16303d5ebcd2f40f12ea5a61e3
Fabio <fabiomarioluna@gmail.com>
2023-10-19 09:30:03 -0300

    correjí el metodo deleteUsuario

--- app/src/main/java/com/prueba/login/daoUsuario.java
+++ app/src/main/java/com/prueba/login/daoUsuario.java
@@ -106,8 +106,9 @@ public class daoUsuario {
         return (sql.update("usuario",cv, "id="+u.getId(),null)>0);
 
     }
-    public  boolean deleteUsuario(int id){
-        return (sql.delete("usuario", "id"+id,null)>0);
+   
+    public boolean deleteUsuario(int id) {
+        return (sql.delete("usuario", "id=?", new String[]{String.valueOf(id)}) > 0);
     }
 
 
